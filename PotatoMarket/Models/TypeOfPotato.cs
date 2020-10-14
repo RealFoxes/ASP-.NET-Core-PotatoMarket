064961507fed20aa77a8503021e63334a105d9e4
Real_Foxes <nikita0630panov@gmail.com>
2020-10-14 17:20:59 +0400

    Add project files.

---
+++ PotatoMarket/Models/TypeOfPotato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PotatoMarket.Models
+{
+	public class TypeOfPotato
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public List<Potato> Potatoes { get; set; }
+		public TypeOfPotato()
+		{
+			Potatoes = new List<Potato>();
+		}
+	}
+}
---
+++ PotatoMarket/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PotatoMarket.Models
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required,MaxLength(32)]
+		public string Login { get; set; }
+		[Required, MaxLength(32)]
+		public string Password { get; set; }
+		[Required,MaxLength(128)]
+		public string FirstName { get; set; }
+		[Required, MaxLength(128)]
+		public string SecondName { get; set; }
+		[Required, Column(TypeName = "date")]
+		public DateTime Birthday { get; set; }
+		[Required]
+		public int Age { get { return DateTime.Now.Year - Birthday.Year; } }
+		public List<Potato> HistoryOfPurchases { get; set; }
+		public List<Potato> CartOfPotato { get; set; }
+		public User()
+		{
+			HistoryOfPurchases = new List<Potato>();
+			CartOfPotato = new List<Potato>();
+		}
+	}
+}
